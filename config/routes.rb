2052d944c302c412fa168522a05ff9dc054cfb66
EC2 Default User <ec2-user@ip-172-31-32-4.us-east-2.compute.internal>
2019-07-14 04:57:11 +0000

    user route

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
 
-  get 'home' => 'pages#home'
+  get '/home' => 'pages#home'
 
-  get 'profile' => 'pages#profile'
+  get '/user/:id' => 'pages#profile'
 
-  get 'explore' => 'pages#explore'
+  get '/explore' => 'pages#explore'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

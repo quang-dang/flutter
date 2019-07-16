76fb666caea28851ffc50ae686b0962763e2fc3d
EC2 Default User <ec2-user@ip-172-31-32-4.us-east-2.compute.internal>
2019-07-16 02:00:52 +0000

    Change values

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],

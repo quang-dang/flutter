cb0430fa7189cc0e12263e06fb91f906db5e0c8d
EC2 Default User <ec2-user@ip-172-31-32-4.us-east-2.compute.internal>
2019-07-16 07:36:27 +0000

    Email set up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['sendgrid_username'],
+    :password       => ENV['sendgrid_password'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }

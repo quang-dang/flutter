51529f5308c3c8b07e54ff898e4fcb97a9782ea0
EC2 Default User <ec2-user@ip-172-31-32-4.us-east-2.compute.internal>
2019-07-17 04:40:08 +0000

    Following Relationships: initial commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         :confirmable
          
-         has_many :posts
+         has_many :posts, dependent: :destroy
+         has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+         has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+         
+         has_many :following, through: :active_relationships, source: :followed
+         has_many :followers, through: :active_relationships, source: :follower
+         
+         def follow(other)
+           active_relationships.create(followed_id: other.id)
+         end
+          
+         def unfollow(other)
+           active_relationships.find_by(followed_id: other.id)
+         end
+         
+         def following(other)
+           following.include?(other)
+         end
 end
